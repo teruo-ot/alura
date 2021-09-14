fd7e28663701b655dab04b1fdcc5d00b9333f398
teruo-ot <marcos.teruo.ouchi@gmail.com>
2021-09-14 12:25:08 -0300

    Apanhando do document.getElementById

--- imersaodev2021/calculadoraMedias/js/scripts.js
+++ imersaodev2021/calculadoraMedias/js/scripts.js
@@ -1,13 +1,23 @@
 // Calcula Médias de quatro notas
 
+var nome = "Teruo";
+
 // Declara as variáveis das notas em cada bimestre
-var notaPrimeiroBimestre = 9
-var notaSegundoBimestre = 4
-var notaTerceiroBimestre = 8
-var notaQuartoBimestre = 10
+var notaPrimeiroBimestre = 9;
+var notaSegundoBimestre = 10;
+var notaTerceiroBimestre = 8;
+var notaQuartoBimestre = 10;
 
 // Calcula a média
-var mediaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4
+var mediaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4;
+
+console.log (mediaFinal);
 
 // Apresenta a média no console, fixando o resultado em uma casa decimal. .toFixed
-console.log (mediaFinal.toFixed(1))
+
+console.log("Bem vindo " + nome);
+
+
+var elementoMedia = document.getElementById ("revelaNota");
+
+console.log (elementoMedia);
