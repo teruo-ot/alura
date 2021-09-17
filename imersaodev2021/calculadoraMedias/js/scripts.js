6209a2b16c3b607ee0a9b1dd9707d3d6d579f6ed
teruo-ot <marcos.teruo.ouchi>
2021-09-17 10:36:47 -0300

    Entrada do nome e das médias à partir do html

--- imersaodev2021/calculadoraMedias/js/scripts.js
+++ imersaodev2021/calculadoraMedias/js/scripts.js
@@ -1,32 +1,30 @@
-// Calcula Médias de quatro notas
+function calculaMedia() { // Calcula Médias de quatro notas
 
-
-// Declara as variáveis das notas em cada bimestre
-var notaPrimeiroBimestre = 9;
-var notaSegundoBimestre = 10;
-var notaTerceiroBimestre = 8;
-var notaQuartoBimestre = 10;
-
-// Calcula a média
-var mediaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre) / 4;
-
-console.log (mediaFinal);
-
-// Apresenta a média no console, fixando o resultado em uma casa decimal. .toFixed
-
-
-function calculaMedia() {
-
+    // Recebe o nome
     var nome = document.getElementById("seuNome").value;
 
-    console.log("Bem vindo " + nome);
+    var elementoNome = document.getElementById("boasVindas");
 
+    var saudacao = "Bem-vindo " + nome;
+
+    elementoNome.innerHTML = saudacao;
+
+
+
+    // Recebe do form html as variáveis das notas em cada bimestre
+    var notaPrimeiroBimestre = parseFloat(document.getElementById("nota1b").value);
+    var notaSegundoBimestre  = parseFloat(document.getElementById("nota2b").value);
+    var notaTerceiroBimestre = parseFloat(document.getElementById("nota3b").value);
+    var notaQuartoBimestre   = parseFloat(document.getElementById("nota4b").value);
+
+    // Calcula a média
+    var mediaFinal = (notaPrimeiroBimestre+notaSegundoBimestre+notaTerceiroBimestre+notaQuartoBimestre)/4;
+
+    // Apresenta a média no console, fixando o resultado em uma casa decimal. .toFixed
 
     var elementoCalculoMedia = document.getElementById("revelaNota");
-    console.log(elementoCalculoMedia);
     
     var mediaCalculada = "Sua média é " + mediaFinal;
-
     elementoCalculoMedia.innerHTML = mediaCalculada
 }
 
