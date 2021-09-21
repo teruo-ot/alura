450e75f5b8e93577363e1dd803d94b3a6540d3b0
teruo-ot <marcos.teruo.ouchi@gmail.com>
2021-09-21 20:49:14 -0300

    Finalizando Mentalista

--- imersaodev2021/mentalista/js/scripts.js
+++ imersaodev2021/mentalista/js/scripts.js
@@ -1,16 +1,30 @@
 var numeroSecreto = parseInt(Math.random() * 11);
+var contador = 0;
 
 function Chutar() {
+    console.log(contador);
+    contador ++;
+
+    var elementoResultado = document.getElementById("resultado")
     var chute = parseInt(document.getElementById("valor").value);
+   
     console.log(numeroSecreto);
 
-    if (chute == numeroSecreto) {
-        console.log("Acertou");
-    } else if (chute > 10 || chute < 0) {
-        console.log ("Você deve digitar um número de 0 a 10");
+    if (contador < 4) {
+        if (chute == numeroSecreto) {
+            elementoResultado.innerHTML = "Tentativa " + contador + ". Acertou!";
+            console.log(elementoResultado);
+        } else if (chute > 10 || chute < 0) {
+        elementoResultado.innerHTML = "Tentativa " + contador + ". Você deve digitar um número de 0 a 10";
+        console.log(elementoResultado);
+        }
+        else { 
+        elementoResultado.innerHTML = "Tentativa " + contador + ". Errou!";
+        console.log(elementoResultado);
+        }
+    } else {
+        elementoResultado.innerHTML = " Suas Tentativas Acabaram!";
     }
-     else { 
-        console.log("Errou!");
-    }
+
 
 }
